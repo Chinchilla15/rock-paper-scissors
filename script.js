b65cc2dfbaf49481ab71c3dd16f1349ffd2ce565
Alessandro Chinchilla <ale030401@gmail.com>
2023-03-24 19:06:18 -0600

    Game done, plays 1 round

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 function getComputerChoice() {
     let randomNumber = Math.random();
     if (randomNumber < 0.33){
-        return "rock";
+        return "Rock";
     } else if (randomNumber < 0.67){
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
 
 let computerSelection = getComputerChoice();
 
-console.log("The selection is:" + computerSelection)
+console.log("The selection is: " + computerSelection)
+
+let selection = prompt("Rock, Paper, Scissor")
+
+function playerSelection(selection){
+    let first = selection.slice(0,1);
+    let rest = selection.slice(1);
+    let lower = rest.toLowerCase();      
+    let upper = first.toUpperCase();
+
+    const cap = upper + lower;
+    return cap;
+}
+
+console.log(playerSelection(selection))
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){ 
+        return "It's a tie!";
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+  console.log(playRound(playerSelection(selection), computerSelection)) //<--(Function, variable)
    
