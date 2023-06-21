fe9802d0fbfa269c565584845df825a4bab10940
Alessandro Chinchilla <ale030401@gmail.com>
2023-06-20 19:24:21 -0600

    Game Done! fixed message issues and refactored the code, making it shorter

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-  const playerScoreBoard = document.querySelector('.p-score');
-  const computerScoreBoard = document.querySelector('.c-score');
-  const result = document.querySelector('.result');
-  const btnClick = document.querySelectorAll('button');
-  const roundsLeft = document.querySelector('.rounds');
-  let playerScore = 0;
-  let computerScore = 0;
-  let rounds = 0;
-  let gameEnded = false;
-
+const playerScoreBoard = document.querySelector('.p-score');
+const computerScoreBoard = document.querySelector('.c-score');
+const result = document.querySelector('.result');
+const buttons = document.querySelectorAll('button');
+const roundsLeft = document.querySelector('.rounds');
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 0;
 
 function getComputerChoice() {
   let randomNumber = Math.random();
@@ -15,120 +13,73 @@ function getComputerChoice() {
     return "Rock";
     } else if (randomNumber < 0.67){
     return "Paper";
-  } else {
+    } else {
     return "Scissors";
-  }
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+    
   if (playerSelection === computerSelection) {
     return "It's a tie!";
   } else if (
-    (playerSelection === "Rock" && computerSelection === "Scissors") ||
-    (playerSelection === "Paper" && computerSelection === "Rock") ||
-    (playerSelection === "Scissors" && computerSelection === "Paper")
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
     return `You win! ${playerSelection} beats ${computerSelection}`;
   } else {
     return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
-}
+  }
 
-
- btnClick.forEach(button => {
+buttons.forEach(button => {
   button.addEventListener('click', ()=>{
+    const playerSelection = button.id.replace('-button','');
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(playerSelection, computerSelection)
+
+    if(roundResult.includes('win')){
+      result.textContent = 'Player Won!';
+      playerScore++;
+      playerScoreBoard.textContent = playerScore;
+    }else if(roundResult.includes('lose')){
+      result.textContent = 'Computer Won!';
+      computerScore++;
+      computerScoreBoard.textContent = computerScore
+    }else{
+      result.textContent = "It's a tie!"
+    }
+
     rounds++;
-    roundsLeft.innerHTML = `Rounds left: ${5 - rounds}`
-
-    if (rounds == 5){
-      gameOver(playerOptions,roundsLeft)
+    roundsLeft.textContent = `Rounds left: ${5 - rounds}`
+    if (rounds === 5){
+      gameOver()
     }
   })
- })
+})
 
- /*
- function updateResult(outcome){
-    result.innerHTML = outcome
+function gameOver() {
+  const reloadBtn = document.querySelector('.reload');
+  const playerOptions = document.querySelector('.boxContainer');
+  playerOptions.style.display = 'none';
+      
+  roundsLeft.style.fontSize = '42px'
+  roundsLeft.innerHTML = "Game Over!";
+
+  if(playerScore > computerScore){
+    result.textContent='You won the game!';
+    result.style.fontSize = '3rem';
+    result.style.color = 'red'
+  }else if (playerScore < computerScore){
+    result.textContent = 'You lost the game!';
+    result.style.fontSize = '3rem';
+    result.style.color = 'red'
+  }else{
+    result.textContent = 'Tie!';
+    result.style.fontSize = '3rem';
+    result.style.color = 'red'
   }
-  */
-
-  const rockButton = document.getElementById('rock-button');
-  rockButton.addEventListener('click',()=> {
-    const computerSelection = getComputerChoice();
-    let rockRound = playRound("Rock", computerSelection);
-    if(rockRound.includes('win')){
-      result.innerHTML = 'Player Won!';
-      playerScore++;
-      playerScoreBoard.textContent = playerScore;
-    }else if(rockRound.includes('lose')){
-      result.innerHTML = 'Computer Won!';
-      computerScore++;
-      computerScoreBoard.textContent = computerScore
-    }else{
-      result.innerHTML = "It's a tie!"
-    }
-    console.log(rockRound)
-  })
-
-  const paperButton = document.getElementById('paper-button');
-  paperButton.addEventListener('click',()=>{
-    const computerSelection = getComputerChoice();
-    let paperRound = playRound("Paper", computerSelection);
-    if(paperRound.includes("win")){
-      result.innerHTML = 'Player Won!';
-      playerScore++;
-      playerScoreBoard.textContent = playerScore;
-    }else if(paperRound.includes('lose')){
-      result.innerHTML = 'Computer Won!';
-      computerScore++;
-      computerScoreBoard.textContent = computerScore
-    }else{
-      result.innerHTML = "It's a tie!";
-    }
-    console.log(paperRound)
-  })
-
-  const scissorsButton = document.getElementById('scissors-button');
-  scissorsButton.addEventListener('click',()=>{
-    const computerSelection = getComputerChoice();
-    let scissorsRound = playRound("Scissors", computerSelection);
-    if(scissorsRound.includes("win")){
-      result.innerHTML = 'Player Won!';
-      playerScore++;
-      playerScoreBoard.textContent = playerScore;
-    }else if(scissorsRound.includes('lose')){
-      result.innerHTML = 'Computer Won!';
-      computerScore++;
-      computerScoreBoard.textContent = computerScore
-    }else{
-      result.innerHTML = "It's a tie!";
-    }
-    console.log(scissorsRound)
-  })
-
-  const playerOptions = [rockButton, paperButton, scissorsButton]
-
-  function gameOver() {
-    const reloadBtn = document.querySelector('.reload');
-    playerOptions.forEach(option =>{
-      option.style.display = 'none';
-    })
-
-    roundsLeft.style.fontSize = '42px'
-    roundsLeft.innerHTML = "Game Over!";
-
-    if(playerScore > computerScore){
-      result.textContent='You won the game!';
-      result.style.fontSize = '3rem';
-      result.style.color = 'red'
-    }else if (playerScore < computerScore){
-      result.textContent = 'You lost the game!';
-      result.style.fontSize = '3rem';
-      result.style.color = 'red'
-    }else{
-      result.textContent = 'Tie!';
-      result.style.fontSize = '3rem';
-      result.style.color = 'red'
-    }
-
-  }
+}
